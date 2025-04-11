119593fe8aa1ea6023fb29ba188ed68697d66cbb
chandan95raj <chandanraaz91@gmail.com>
2025-04-11 17:30:51 +0530

    blog-details page-11Apr2025

--- resources/views/web/blog-details.blade.php
+++ resources/views/web/blog-details.blade.php
@@ -1,14 +1,18 @@
 @extends('web.app')
 
 @section('content')
+    <style>
+        .blog-main-img img{
+            height: 300px;
+            object-fit: cover;
+        }
+    </style>
     <section class=" d-table w-100 overflow-hidden px-0 pt-5 my-5 mb-0" id="blog_header">
         <div class="container">
             <div class="row align-items-center">
                 <div class="col-lg-12 col-md-12 text-center ">
                     <div class="title-heading">
                         <h4 class="heading "><span class="text-primary fw-bold">Blogs Details</span></h4>
-                        <!--<p class="para-desc text-muted"><span class="text-primary fw-bold">PurnPay </span> has integrated with all the available banks to enable cash withdrawal and other AEPS services for all its partners.</p>-->
-
                     </div>
                 </div><!--end col-->
 
@@ -18,13 +22,13 @@
 
     <section class="blogs_main mb-3">
         <div class="container pt-lg-4">
-            <div class="w-100">
-                <img src="{{ asset('admin/uploads/' . $singleblog->ns_img) }}" class="w-100" alt="">
+            <div class="text-center blog-main-img">
+                <img src="{{ asset('admin/uploads/' . $singleblog->ns_img) }}" style="width: auto !important" class="shadow-md rounded" alt="{{ $singleblog->ns_title }}">
             </div>
-            <h3 class="my-3">{{$singleblog->ns_title}}
+            <h3 class="my-3">{{ $singleblog->ns_title }}
             </h3>
             <div>
-                {!!$singleblog->ns_desc!!}
+                {!! $singleblog->ns_desc !!}
             </div>
         </div>
     </section>
